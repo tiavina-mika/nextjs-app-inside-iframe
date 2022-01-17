f2474b6c8c85df51acb542386fc13c050bb5b556
TiavinaMichael <ralainirinatiavina@gmail.com>
2022-01-17 16:57:38 +0300

    send data to the iframe

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import { useRef } from "react";
 import "./styles.css";
 
+const APP_URL = "http://localhost:3000";
 const App = () => {
+  let Iref = useRef("");
+
+  const sendMessage = () => {
+    if (!Iref.current) return;
+    Iref.current.contentWindow.postMessage(
+      { message: window.location.host },
+      APP_URL
+    );
+  };
+
+  // useEffect(() => {
+  //   if (!Iref.current) return;
+  //   Iref.current.contentWindow.postMessage(
+  //     { message: "test" }, APP_URL);
+  // console.log('cool', Iref.current.contentWindow.postMessage)
+  // }, [])
   return (
     <div className="App">
+      <button onClick={sendMessage}>Send message to iframe</button>
       <iframe
-        src="http://localhost:3000"
+        ref={Iref}
+        src={APP_URL}
         height="100%"
         width="100%"
         scrolling="no"
