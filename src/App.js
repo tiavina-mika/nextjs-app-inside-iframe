1ea0c6da113b6c2340877a4a835114b057683b22
TiavinaMichael <ralainirinatiavina@gmail.com>
2022-01-17 16:31:41 +0300

    fix iframe url

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ const App = () => {
   return (
     <div className="App">
       <iframe
-         src="http://localhost:3000?param1=value1"
-         height="100%"
-         width="100%"
-         scrolling="no"
-         frameBorder='0'
-         title="nextJs"
-        >
-      </iframe>
+        src="http://localhost:3000"
+        height="100%"
+        width="100%"
+        scrolling="no"
+        frameBorder="0"
+        title="nextJs"
+      ></iframe>
     </div>
   );
 };
